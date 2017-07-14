69b55f973f66f7ffbe449e1e87e42e882cbbebdb
Timur Saglam <uleay@student.kit.edu>
2017-07-14 11:56:55 +0200

    EDIT: Improved the package filter functionality.

--- src/main/java/jce/util/PackageFilter.java
+++ src/main/java/jce/util/PackageFilter.java
@@ -14,7 +14,7 @@ import org.eclipse.jdt.core.JavaModelException;
  * Utility class that allows to retrieve a specific packages of an Eclipse Java project.
  * @author Timur Saglam
  */
-public class PackageFilter {
+public final class PackageFilter {
     private static final Logger logger = LogManager.getLogger(PackageFilter.class.getName());
 
     private PackageFilter() {
@@ -22,22 +22,60 @@ public class PackageFilter {
     }
 
     /**
-     * Retrieves all {@link IPackageFragment}s of a specific {@link IProject} that start with a specific prefix.
+     * Retrieves all {@link IPackageFragment}s of a specific {@link IProject} that do not start with any prefix of a
+     * specific set of prefixes.
      * @param project is the specific {@link IProject}.
-     * @param prefix is the prefix {@link String}.
+     * @param prefixes is the specific set of prefixes.
      * @return a list of {@link IPackageFragment}s that start with a specific prefix.
      */
-    public static List<IPackageFragment> startsWith(IProject project, String prefix) {
+    public static List<IPackageFragment> startsNotWith(IProject project, String... prefixes) {
         List<IPackageFragment> filteredFragments = new LinkedList<>();
-        try {
-            for (IPackageFragment fragment : JavaCore.create(project).getPackageFragments()) { // get packages
-                if (fragment.getElementName().startsWith(prefix)) {
-                    filteredFragments.add(fragment); // filter packages
-                }
+        for (IPackageFragment fragment : retrievePackages(project)) { // get packages
+            if (!startsWith(fragment, prefixes)) {
+                filteredFragments.add(fragment); // filter packages
             }
+        }
+        return filteredFragments;
+    }
+
+    /**
+     * Retrieves all {@link IPackageFragment}s of a specific {@link IProject} that start with at least one of a specific
+     * set of prefixes.
+     * @param project is the specific {@link IProject}.
+     * @param prefixes is the specific set of prefixes.
+     * @return a list of {@link IPackageFragment}s that start with a specific prefix.
+     */
+    public static List<IPackageFragment> startsWith(IProject project, String... prefixes) {
+        List<IPackageFragment> filteredFragments = new LinkedList<>();
+        for (IPackageFragment fragment : retrievePackages(project)) { // get packages
+            if (startsWith(fragment, prefixes)) {
+                filteredFragments.add(fragment); // filter packages
+            }
+        }
+        return filteredFragments;
+    }
+
+    /**
+     * Retrieves every {@link IPackageFragment} from a specific {@link IProject}.
+     */
+    private static IPackageFragment[] retrievePackages(IProject project) {
+        try {
+            return JavaCore.create(project).getPackageFragments();
         } catch (JavaModelException exception) {
             logger.fatal(exception);
         }
-        return filteredFragments;
+        return null;
+    }
+
+    /**
+     * Checks whether a package fragment starts with at least one of a defined set of prefixes.
+     */
+    private static boolean startsWith(IPackageFragment fragment, String... prefixes) {
+        for (String prefix : prefixes) {
+            if (fragment.getElementName().startsWith(prefix)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
