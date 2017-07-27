3a6a27a941b6fb094a78354cd0fb11dca9787f92
Timur Saglam <uleay@student.kit.edu>
2017-07-27 16:35:39 +0200

    EDIT: Improved logging.

--- src/main/java/jce/util/ResourceRefresher.java
+++ src/main/java/jce/util/ResourceRefresher.java
@@ -28,7 +28,7 @@ public final class ResourceRefresher {
     public static void refresh(IResource resource) {
         try {
             resource.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor()); // not detailed logging
-            logger.info("Refreshed IResource " + resource.getName()); // simple logging
+            logger.info("Refreshed " + resource.getClass().getSimpleName() + " " + resource.getName());
         } catch (CoreException exception) {
             logger.warn("Could not refresh. Try refreshing manually!", exception);
         }
