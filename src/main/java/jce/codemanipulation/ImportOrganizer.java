dceee7f5851cc2a9ddeb13d688ca22c26985f3aa
Timur Saglam <uleay@student.kit.edu>
2017-08-20 10:48:41 +0200

    FIX: Updated ImportOrganizer to support Eclipse Oxygen packages. The method applyEdits() in the class JavaModelUtil was removed with in Oxygen.

--- src/main/java/jce/codemanipulation/ImportOrganizer.java
+++ src/main/java/jce/codemanipulation/ImportOrganizer.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.internal.corext.CorextMessages;
+import org.eclipse.jdt.internal.corext.ValidateEditException;
 import org.eclipse.jdt.internal.corext.codemanipulation.OrganizeImportsOperation;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
+import org.eclipse.jdt.internal.corext.util.Resources;
 import org.eclipse.text.edits.TextEdit;
 
 import jce.properties.EcorificationProperties;
@@ -47,18 +52,41 @@ public class ImportOrganizer extends AbstractCodeManipulator {
 
     @Override
     protected void manipulate(ICompilationUnit unit) throws JavaModelException {
-        unit.becomeWorkingCopy(monitor); // changes compilation unit handle to working copy
-        CompilationUnit reconciledUnit = unit.reconcile(AST.JLS8, false, null, monitor); // don't ask me
+        unit.becomeWorkingCopy(monitor); // changes unit handle to working copy
+        CompilationUnit reconciledUnit = unit.reconcile(AST.JLS8, false, null, monitor); // Don't ask me.
         try {
             OrganizeImportsOperation operation = new OrganizeImportsOperation(unit, reconciledUnit, true, true, true, null);
             TextEdit edit = operation.createTextEdit(monitor);
-            JavaModelUtil.applyEdit(unit, edit, true, monitor);
+            applyEdit(unit, edit);
             unit.commitWorkingCopy(true, monitor);
             unit.save(monitor, true);
         } catch (OperationCanceledException exception) {
             logger.error(exception);
         } catch (CoreException exception) {
             logger.error(exception);
+        }
+    }
+
+    /**
+     * Applies {@link TextEdit} to {@link ICompilationUnit} and saves the changes. Taken from JavaModelUtil, because
+     * {@link JavaModelUtil#applyEdit()} applyEdit is gone in Oxygen.
+     */
+    private void applyEdit(ICompilationUnit unit, TextEdit edit) throws CoreException, ValidateEditException {
+        IFile file = (IFile) unit.getResource();
+        if (!file.exists()) {
+            unit.applyTextEdit(edit, monitor);
+        } else {
+            monitor.beginTask(CorextMessages.JavaModelUtil_applyedit_operation, 2);
+            try {
+                IStatus status = Resources.makeCommittable(file, null);
+                if (!status.isOK()) {
+                    throw new ValidateEditException(status);
+                }
+                unit.applyTextEdit(edit, monitor);
+                unit.save(monitor, true);
+            } finally {
+                monitor.done();
+            }
         }
     }
 }
