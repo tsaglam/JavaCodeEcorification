afba5d5d9521b3c90bb47ad8f4a04b6e5ab9edd4
Timur Saglam <uleay@student.kit.edu>
2017-09-01 15:54:11 +0200

    EDIT: Implemented the addition of superinterface relations from origin code interfaces to their correlating Ecore interface.

--- src/main/java/jce/codemanipulation/origin/InheritanceManipulationVisitor.java
+++ src/main/java/jce/codemanipulation/origin/InheritanceManipulationVisitor.java
@@ -1,5 +1,6 @@
 package jce.codemanipulation.origin;
 
+import static jce.properties.TextProperty.ECORE_PACKAGE;
 import static jce.properties.TextProperty.WRAPPER_PACKAGE;
 import static jce.properties.TextProperty.WRAPPER_PREFIX;
 import static jce.properties.TextProperty.WRAPPER_SUFFIX;
@@ -9,10 +10,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 import jce.properties.EcorificationProperties;
+import jce.util.PathHelper;
 import jce.util.logging.MonitorFactory;
 
 /**
@@ -21,8 +24,9 @@ import jce.util.logging.MonitorFactory;
  */
 public class InheritanceManipulationVisitor extends ASTVisitor {
     private final String currentPackage;
-    private final EcorificationProperties properties;
     private final IProgressMonitor monitor;
+    private final PathHelper nameUtil;
+    private final EcorificationProperties properties;
 
     /**
      * Basic constructor.
@@ -33,41 +37,54 @@ public class InheritanceManipulationVisitor extends ASTVisitor {
         this.currentPackage = currentPackage;
         this.properties = properties;
         monitor = MonitorFactory.createProgressMonitor(LogManager.getLogger(getClass().getName()), properties);
+        nameUtil = new PathHelper('.');
     }
 
     @Override
     public boolean visit(TypeDeclaration node) {
-        if (!node.isInterface()) { // if is class, manipulate inheritance:
-            setSuperClass(node, getPackage() + "." + getName(node));
-            monitor.beginTask("Changed super type of " + node.getName().getIdentifier(), 0);
+        if (node.isInterface()) {
+            addEcoreInterface(node); // add correlating Ecore interface as super interface
+        } else {
+            overrideSuperclass(node); // set correlating wrapper as super class
         }
         return false;
     }
 
     /**
+     * Adds the correlating Ecore interface of an {@link TypeDeclaration} as super interface. Other superinterfaces are
+     * being retained.
+     */
+    @SuppressWarnings("unchecked")
+    private void addEcoreInterface(TypeDeclaration node) {
+        String name = nameUtil.append(properties.get(ECORE_PACKAGE), currentPackage, node.getName().getIdentifier());
+        node.superInterfaceTypes().add(nameToType(node, name));
+        monitor.beginTask("Added Ecore super interface to " + node.getName().getIdentifier(), 0);
+    }
+
+    /**
      * Returns the name of the super class.
      */
-    private String getName(TypeDeclaration node) {
-        return properties.get(WRAPPER_PREFIX) + node.getName().toString() + properties.get(WRAPPER_SUFFIX);
+    private String getWrapperName(TypeDeclaration node) {
+        return properties.get(WRAPPER_PREFIX) + node.getName().getIdentifier() + properties.get(WRAPPER_SUFFIX);
     }
 
     /**
-     * Returns the package path of the super class.
+     * Builds an {@link SimpleType} out of an fully qualified name and a {@link TypeDeclaration}.
      */
-    private String getPackage() {
-        return properties.get(WRAPPER_PACKAGE) + "." + currentPackage;
-    }
-
-    /**
-     * Sets a qualified name as super class of a type declaration.
-     * @param declaration is the type declaration.
-     * @param qualifiedName is the qualified name.
-     */
-    private void setSuperClass(TypeDeclaration declaration, String qualifiedName) {
+    private Type nameToType(TypeDeclaration declaration, String qualifiedName) {
         AST ast = declaration.getAST();
         Name name = ast.newName(qualifiedName);
-        Type type = ast.newSimpleType(name);
-        declaration.setSuperclassType(type);
+        return ast.newSimpleType(name);
+    }
+
+    /**
+     * Sets the correlating wrapper class of an {@link TypeDeclaration} as the super class. This overrides the old super
+     * class.
+     */
+    private void overrideSuperclass(TypeDeclaration node) {
+        String name = nameUtil.append(properties.get(WRAPPER_PACKAGE), currentPackage, getWrapperName(node));
+        node.setSuperclassType(nameToType(node, name));
+        monitor.beginTask("Changed super type of " + node.getName().getIdentifier(), 0);
     }
 
 }
