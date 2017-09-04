a9be5ebbb0798ee46ddf50831b71bbda40be3d49
Timur Saglam <uleay@student.kit.edu>
2017-09-04 10:56:39 +0200

    EDIT: Removed unnecessary Eclipse Neon API method that could be replaced by a method in the class AbstractCodeManipulator.

--- src/main/java/jce/codemanipulation/ImportOrganizer.java
+++ src/main/java/jce/codemanipulation/ImportOrganizer.java
@@ -4,22 +4,16 @@ import java.util.List;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.internal.corext.CorextMessages;
-import org.eclipse.jdt.internal.corext.ValidateEditException;
 import org.eclipse.jdt.internal.corext.codemanipulation.OrganizeImportsOperation;
-import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
-import org.eclipse.jdt.internal.corext.util.Resources;
 import org.eclipse.text.edits.TextEdit;
 
 import jce.properties.EcorificationProperties;
@@ -45,29 +39,6 @@ public class ImportOrganizer extends AbstractCodeManipulator {
         monitor = MonitorFactory.createProgressMonitor(logger, properties);
     }
 
-    /**
-     * Applies {@link TextEdit} to {@link ICompilationUnit} and saves the changes. Taken from JavaModelUtil, because
-     * {@link JavaModelUtil#applyEdit()} applyEdit is gone in Oxygen.
-     */
-    private void applyEdit(ICompilationUnit unit, TextEdit edit) throws CoreException, ValidateEditException {
-        IFile file = (IFile) unit.getResource();
-        if (!file.exists()) {
-            unit.applyTextEdit(edit, monitor);
-        } else {
-            monitor.subTask(CorextMessages.JavaModelUtil_applyedit_operation);
-            try {
-                IStatus status = Resources.makeCommittable(file, null);
-                if (!status.isOK()) {
-                    throw new ValidateEditException(status);
-                }
-                unit.applyTextEdit(edit, monitor);
-                unit.save(monitor, true);
-            } finally {
-                monitor.done();
-            }
-        }
-    }
-
     @Override
     protected List<IPackageFragment> filterPackages(IProject project, EcorificationProperties properties) {
         return PackageFilter.startsNotWith(project, properties.get(TextProperty.WRAPPER_PACKAGE));
@@ -80,9 +51,7 @@ public class ImportOrganizer extends AbstractCodeManipulator {
         try {
             OrganizeImportsOperation operation = new OrganizeImportsOperation(unit, reconciledUnit, true, true, true, null);
             TextEdit edit = operation.createTextEdit(monitor);
-            applyEdit(unit, edit);
-            unit.commitWorkingCopy(true, monitor);
-            unit.save(monitor, true);
+            applyTextEdit(edit, unit);
         } catch (OperationCanceledException exception) {
             logger.error(exception);
         } catch (CoreException exception) {
