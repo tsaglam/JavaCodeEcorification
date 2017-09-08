4f927f0786557ff66989ebabcf0e22feeaceaf54
Timur Saglam <uleay@student.kit.edu>
2017-09-08 15:41:13 +0200

    ADD: Class for exposing inner classes with the recently added visitor.

---
+++ src/main/java/jce/codemanipulation/origin/InnerClassExposer.java
@@ -0,0 +1,26 @@
+package jce.codemanipulation.origin;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
+
+import jce.properties.EcorificationProperties;
+
+/**
+ * Changes the visibility of default inner classes of the origin code to public to make them visible.
+ * @author Timur Saglam
+ */
+public class InnerClassExposer extends OriginCodeManipulator {
+
+    /**
+     * Simple constructor that sets the properties.
+     * @param properties are the {@link EcorificationProperties}.
+     */
+    public InnerClassExposer(EcorificationProperties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected void manipulate(ICompilationUnit unit) throws JavaModelException {
+        applyVisitorModifications(unit, new ClassExpositionVisitor(properties));
+    }
+}
