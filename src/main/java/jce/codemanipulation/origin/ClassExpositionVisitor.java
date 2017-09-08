f18a6d5d3f50bbe348691dc71cfcce0518c4fd6f
Timur Saglam <uleay@student.kit.edu>
2017-09-08 15:40:53 +0200

    FIX: The class exposition visitor is now working correctly.

--- src/main/java/jce/codemanipulation/origin/ClassExpositionVisitor.java
+++ src/main/java/jce/codemanipulation/origin/ClassExpositionVisitor.java
@@ -1,12 +1,17 @@
 package jce.codemanipulation.origin;
 
-import static org.eclipse.jdt.core.dom.Modifier.ModifierKeyword.DEFAULT_KEYWORD;
 import static org.eclipse.jdt.core.dom.Modifier.ModifierKeyword.PUBLIC_KEYWORD;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import jce.properties.EcorificationProperties;
+import jce.util.logging.MonitorFactory;
 
 /**
  * {@link ASTVisitor} that makes all inner classes that have default visibility visible through changing the visibility
@@ -14,23 +19,42 @@ import org.eclipse.jdt.core.dom.TypeDeclaration;
  * @author Timur Saglam
  */
 public class ClassExpositionVisitor extends ASTVisitor {
+    private static final Logger logger = LogManager.getLogger(ClassExpositionVisitor.class.getName());
+    private IProgressMonitor monitor;
+
+    /**
+     * Basic constructor.
+     * @param properties are the Ecorification properties.
+     */
+    public ClassExpositionVisitor(EcorificationProperties properties) {
+        monitor = MonitorFactory.createProgressMonitor(logger, properties);
+    }
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean visit(TypeDeclaration node) {
-        if (isDefaultInnerClass(node)) { // if is default nested class
-            AST ast = node.getAST(); // make public:
-            node.modifiers().remove(ast.newModifier(DEFAULT_KEYWORD));
-            node.modifiers().add(ast.newModifier(PUBLIC_KEYWORD));
+        if (isInnerClass(node) && isDefault(node)) { // if is default inner class
+            AST ast = node.getAST();
+            Modifier modifier = ast.newModifier(PUBLIC_KEYWORD); // create public modifier
+            node.modifiers().add(modifier); // add to type declaration
+            monitor.beginTask("Exposed: " + node.getName().getFullyQualifiedName(), 0);
         }
         return super.visit(node);
     }
 
     /**
-     * Checks whether a {@link TypeDeclaration} is a default inner class, which means it is a member type, no interface,
-     * and has the access level modifier default.
+     * Checks whether a {@link TypeDeclaration} is default and not static, which means it does not have the modifiers
+     * public, private, protected or static.
      */
-    private boolean isDefaultInnerClass(TypeDeclaration node) {
-        return node.isMemberTypeDeclaration() && !node.isInterface() && Modifier.isDefault(node.getModifiers());
+    private boolean isDefault(TypeDeclaration node) {
+        int flags = node.getModifiers();
+        return !Modifier.isPublic(flags) && !Modifier.isProtected(flags) && !Modifier.isPrivate(flags) && !Modifier.isStatic(flags);
+    }
+
+    /**
+     * Checks whether a {@link TypeDeclaration} is a inner class, which means it is a member type and not an interface.
+     */
+    private boolean isInnerClass(TypeDeclaration node) {
+        return node.isMemberTypeDeclaration() && !node.isInterface(); // is member class
     }
 }
