d6dd6dde56a2b89e4c71a5c7a649e61db096a1e6
Timur Saglam <uleay@student.kit.edu>
2017-12-14 23:09:50 +0100

    EDIT: Further improved the default constructor generator.

--- src/main/java/jce/codemanipulation/origin/DefaultConstructorGenerator.java
+++ src/main/java/jce/codemanipulation/origin/DefaultConstructorGenerator.java
@@ -6,10 +6,14 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
+import org.eclipse.jdt.core.dom.IExtendedModifier;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 import jce.properties.EcorificationProperties;
+import jce.util.RawTypeUtil;
 import jce.util.jdt.ASTUtil;
 
 /**
@@ -39,9 +43,13 @@ public class DefaultConstructorGenerator extends OriginCodeManipulator {
     private class ConstructorGenerationVisitor extends ASTVisitor {
 
         @Override
-        public boolean visit(TypeDeclaration node) { // TODO (HIGH) what if def const is private?
-            if (node.isPackageMemberTypeDeclaration() && !hasDefaultConstructor(node)) {
-                addDefaultConstructor(node);
+        public boolean visit(TypeDeclaration node) {
+            if (node.isPackageMemberTypeDeclaration() && hasConstructor(node)) {
+                if (hasDefaultConstructor(node)) {
+                    ensureVisibility(node); // make sure default constructor is public
+                } else {
+                    addDefaultConstructor(node); // add new default constructor
+                }
             }
             return false;
         }
@@ -61,16 +69,66 @@ public class DefaultConstructorGenerator extends OriginCodeManipulator {
         }
 
         /**
-         * Checks whether a type has a default constructor by checking whether one of its methods both is a constructor
-         * and has no parameters.
+         * Checks whether the default constructor of a {@link TypeDeclaration} is public, makes it public if it is not.
          */
-        private boolean hasDefaultConstructor(TypeDeclaration type) {
+        @SuppressWarnings("unchecked")
+        private void ensureVisibility(TypeDeclaration node) {
+            MethodDeclaration constructor = getDefaultConstructor(node);
+            if (!Modifier.isPublic(constructor.getModifiers())) {
+                removeModifiers(constructor);
+                Modifier modifier = node.getAST().newModifier(PUBLIC_KEYWORD);
+                constructor.modifiers().add(modifier); // make public
+            }
+        }
+
+        /**
+         * Finds the explicit default constructor of a {@link TypeDeclaration}, by finding the method that both is a
+         * constructor and has no parameters. Returns null if no default constructor was found.
+         */
+        private MethodDeclaration getDefaultConstructor(TypeDeclaration type) {
             for (MethodDeclaration method : type.getMethods()) {
                 if (method.isConstructor() && method.parameters().isEmpty()) {
+                    return method;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Checks whether a {@link TypeDeclaration} has at least one constructor.
+         */
+        private boolean hasConstructor(TypeDeclaration type) {
+            for (MethodDeclaration method : type.getMethods()) {
+                if (method.isConstructor()) {
                     return true;
                 }
             }
             return false;
+        }
+
+        /**
+         * Checks whether a type has a explicit default constructor by checking whether one of its methods both is a
+         * constructor and has no parameters.
+         */
+        private boolean hasDefaultConstructor(TypeDeclaration type) {
+            return getDefaultConstructor(type) != null;
+        }
+
+        /**
+         * Removes private and protected keyword in form of a {@link Modifier} from a type declaration node. Returns the
+         * removed {@link Modifier} or null of none was removed.
+         */ // TODO (HIGH) remove duplicate code with ClassExpositionVisitor
+        private Modifier removeModifiers(BodyDeclaration node) {
+            for (IExtendedModifier extendedModifier : RawTypeUtil.castList(IExtendedModifier.class, node.modifiers())) {
+                if (extendedModifier.isModifier()) { // if is modifier (not annotation)
+                    Modifier modifier = (Modifier) extendedModifier; // cast to modifier
+                    if (modifier.isPrivate() || modifier.isProtected()) {
+                        modifier.delete(); // remove modifier from node
+                        return modifier;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
